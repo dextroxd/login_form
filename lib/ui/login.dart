13c187395f927cd7165e04227d59f85d89cbfe08
dextroxd <divyanshu171299@gmail.com>
2019-03-27 13:50:50 +0530

    Initial Commit

---
+++ lib/ui/login.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return new LoginState();
+  }
+}
+
+class LoginState extends State<Login> {
+  final TextEditingController _userController = new TextEditingController();
+  final TextEditingController _passController = new TextEditingController();
+  String _welcomeString = "";
+
+  void _erase() {
+    setState(() {
+      _userController.clear();
+      _passController.clear();
+    });
+  }
+
+  void _showWelcome() {
+    setState(() {
+      if (_userController.text.isNotEmpty && _passController.text.isNotEmpty) {
+        _welcomeString =  _userController.text;
+      }
+      else{
+        _welcomeString = "Nothing";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Login"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.blueGrey,
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new Column(
+          children: <Widget>[
+            //image/profile
+            new Image.asset(
+              'images/face.png',
+              width: 90.0,
+              height: 90.0,
+              color: Colors.greenAccent,
+            ),
+            new Container(
+              height: 180.0,
+              width: 380.0,
+              color: Colors.white,
+              child: new Column(
+                children: <Widget>[
+                  new TextField(
+                    controller: _userController,
+                    decoration: new InputDecoration(
+                      hintText: 'Username',
+                      icon: new Icon(Icons.person),
+                    ),
+                  ),
+                  new TextField(
+                    controller: _passController,
+                    decoration: new InputDecoration(
+                      hintText: 'Password',
+                      icon: new Icon(Icons.lock),
+                    ),
+                  ),
+                  new Padding(padding: new EdgeInsets.all(10.5)),
+                  new Center(
+                    child: new Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+//
+                        new Container(
+                          margin: const EdgeInsets.only(right: 20.0),
+                          child: new RaisedButton(
+                            onPressed:_showWelcome,
+                            child: new Text(
+                              "Login",
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 16.9),
+                            ),
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                        new Container(
+                          margin: const EdgeInsets.only(left: 20.0),
+                          child: new RaisedButton(
+                            onPressed: _erase,
+                            child: new Text(
+                              "Clear",
+                              style: new TextStyle(
+                                  color: Colors.white, fontSize: 16.9),
+                            ),
+                            color: Colors.redAccent,
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            new Padding(padding: new EdgeInsets.all(14.0)),
+
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                new Text(
+                  "Welcome , $_welcomeString",
+                  style: new TextStyle(
+                      color: Colors.white,
+                      fontSize: 19.4,
+                      fontWeight: FontWeight.w500),
+                )
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
